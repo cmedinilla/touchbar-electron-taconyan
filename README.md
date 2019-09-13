b01588663efa5656926fd2ae612cfa081e6cf2b2
cris.medinill@gmail.com <cris.medinill@gmail.com>
2019-09-13 18:42:47 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Touch bar Elextron Mexi Nyan
+
+run Mexi Nyan:
+```bash
+npm install
+npm start
+```
---
+++ mexnyancat.js
@@ -0,0 +1,49 @@
+const {app, BrowserWindow, TouchBar} = require('electron')
+
+const {TouchBarLabel, TouchBarButton, TouchBarSpacer} = TouchBar
+
+let window
+
+const touchBarButton =   new TouchBarButton ({
+  label:'',
+  backgroundColor: '#000000',
+  icon: 'images/mexinyan/mexinyan1.png',
+})
+
+const touchBar = new TouchBar({
+  items: [touchBarButton]
+})
+
+let frame = 0;
+
+const updateFrames = () => {
+  if (frame === 12) {
+    frame = 1;
+  } else {
+    frame += 1;
+  }
+  const nyanPath = `images/mexinyan/mexinyan${frame}.png`;
+  touchBarButton.icon = nyanPath;
+}
+
+const animateFrames = () => {
+  setInterval(updateFrames, 80)
+};
+
+app.once('ready', () => {
+  window = new BrowserWindow({
+    frame: false,
+    titleBarStyle: 'hiddenInset',
+    width: 200,
+    height: 200,
+    backgroundColor: '#0000'
+  })
+  window.loadURL('http://www.nyan.cat/index.php?cat=mexinyan')
+  window.setTouchBar(touchBar)
+  animateFrames()
+})
+
+
+app.on('window-all-closed', () => {
+  app.quit();
+});
